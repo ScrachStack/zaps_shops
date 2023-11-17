a6f0ba0fc5b6d5c1f1b9341cb583d0e25925263b
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-11-16 21:22:37 -0600

    Create edit.lua

---
+++ edit.lua
@@ -0,0 +1,13 @@
+config = {
+    Stores = {
+        {
+            name = "Grove Store",
+            position = vector3(-47.8585, -1757.2069, 29.4210),
+            items = {
+                { label = "ammo", value = "ammo", price = 5 },
+                { label = "armor", value = "armor", price = 3 }
+            }
+        },
+    },
+	menuPosition = "center"
+}
